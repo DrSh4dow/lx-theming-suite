c37da5cfd0e5e83fdb4fdb6a7f32a64c5966db98
drsh4dow <daniel.morettiv@gmail.com>
2023-12-31 20:30:17 -0300

    improved mobile layout

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -6,7 +6,7 @@
 	class="grid h-screen w-screen grid-rows-[64px_1fr] overflow-clip border-red-500 bg-background text-foreground"
 >
 	<Header />
-	<div class="grid grid-cols-[40px_1fr] sm:grid-cols-[280px_1fr]">
+	<div class="grid grid-cols-[64px_1fr] md:grid-cols-[280px_1fr]">
 		<nav class="h-full border-r"></nav>
 		<section class="overflow-y-scroll p-4 sm:p-8">
 			<slot />
--- src/routes/(app)/Header.svelte
+++ src/routes/(app)/Header.svelte
@@ -1,14 +1,15 @@
 <script>
 	import { currentTheme } from '$lib/stores/themeProvider';
+	import { cn } from '$lib/utils/cn';
 	import { MoonIcon, PaletteIcon, SunIcon } from 'lucide-svelte';
 </script>
 
 <header
-	class="grid w-full grid-cols-[40px_1fr] justify-center border-b sm:grid-cols-[280px_1fr] sm:justify-normal"
+	class="grid w-full grid-cols-[64px_1fr] justify-center border-b sm:justify-normal md:grid-cols-[280px_1fr]"
 >
-	<div class="flex items-center justify-start gap-x-2 border-r px-4 sm:px-8">
+	<div class="flex items-center justify-center gap-x-2 border-r md:justify-start md:px-8">
 		<PaletteIcon class="h-6 w-6" />
-		<h5 class="hidden select-none font-semibold sm:block">Linux Theming Suite</h5>
+		<h5 class="hidden select-none font-semibold md:block">Linux Theming Suite</h5>
 	</div>
 	<div class="flex items-center justify-between px-4 sm:px-8">
 		<input
@@ -29,7 +30,11 @@
 						class="peer sr-only"
 					/>
 					<div
-						class="peer h-5 w-10 cursor-pointer rounded-full bg-muted transition-colors after:absolute after:start-[4px] after:top-[2px] after:h-4 after:w-4 after:rounded-full after:bg-background after:transition-all after:content-[''] peer-checked:after:translate-x-full peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-muted-foreground rtl:peer-checked:after:-translate-x-full"
+						class={cn(
+							'peer h-5 w-10 cursor-pointer rounded-full bg-muted transition-colors',
+							"after:absolute after:start-[4px] after:top-[2px] after:h-4 after:w-4 after:rounded-full after:bg-background after:transition-all after:content-[''] ",
+							'peer-checked:bg-foreground peer-checked:after:translate-x-full peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-muted-foreground rtl:peer-checked:after:-translate-x-full'
+						)}
 					/>
 				</div>
 				<SunIcon class="h-4 w-4" />
